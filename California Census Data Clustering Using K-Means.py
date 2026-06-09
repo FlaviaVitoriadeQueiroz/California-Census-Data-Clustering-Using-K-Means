a147ccdd1af943ecc44c3b4b0c67317b2132210a
Flávia Vitória de Queiroz <flaviavitoriaqueiroz@gmail.com>
2026-06-08 21:46:21 -0300

    Remove import of fetch_california_housing from the script

--- California Census Data Clustering Using K-Means.py
+++ California Census Data Clustering Using K-Means.py
@@ -8,7 +8,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.datasets import fetch_california_housing
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 from sklearn.impute import SimpleImputer
